70e925d32797671011e9046afbc3441e468e3272
Mel Incognito <melxincognito@gmail.com>
2022-02-02 20:48:13 -0700

    able to grab info input into forms

--- ClientApp/src/components/forms/AddIngredientsForm.jsx
+++ ClientApp/src/components/forms/AddIngredientsForm.jsx
@@ -3,46 +3,29 @@ import React, { useState } from "react";
 import { Button, FormControl } from "react-bootstrap";
 
 export default function AddIngredientsForm() {
-  const [ingredientInputFields, setIngredientInputFields] = useState([
-    {
-      ingredientName: "",
-      ingredientMeasurement: "",
-    },
-    {
-      ingredientName: "",
-      ingredientMeasurement: "",
-    },
-  ]);
+  const [ingredientInputField, setIngredientInputField] = useState("");
+
+  // grabbing info from the form and able to put it on the screen
 
   const pressedButton = () => {
-    console.log(ingredientInputFields.ingredientName.value);
+    console.log(ingredientInputField);
   };
 
-  const handleChangeInput = (index, event) => {
-    const values = [...ingredientInputFields];
-    values[index][event.target.ingredientName] = event.target.value;
-    setIngredientInputFields(values);
-  };
+  // for ingredientName form control it's holding the value on the onChange to set the empty
+  // state of ingredientInputField to the state of whatever you put in
 
   return (
     <>
       <form>
-        {ingredientInputFields.map((ingredientInputField, index) => (
-          <div key={index}>
-            <FormControl
-              id="ingredientName"
-              placeholder="Ingredient Name"
-              value={ingredientInputField.ingredientName}
-              onChange={(event) => handleChangeInput(index, event)}
-            />
-            <FormControl
-              id="ingredientMeasurement"
-              placeholder="Amount"
-              value={ingredientInputField.ingredientMeasurement}
-              onChange={(event) => handleChangeInput(index, event)}
-            />
-          </div>
-        ))}
+        <div>
+          <FormControl
+            id="ingredientName"
+            placeholder="Ingredient Name"
+            onChange={(e) => setIngredientInputField(e.target.value)}
+          />
+          <FormControl id="ingredientMeasurement" placeholder="Amount" />
+        </div>
+
         <Button onClick={pressedButton}> Press me</Button>
       </form>
     </>
